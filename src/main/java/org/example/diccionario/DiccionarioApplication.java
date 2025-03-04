18710b07a102d5835d7fc6c24e77fb4a0d645aa7
Rafa <luquegomezrafa6@gmail.com>
2025-03-04 18:05:50 +0100

    first commit

---
+++ src/main/java/org/example/diccionario/DiccionarioApplication.java
@@ -0,0 +1,13 @@
+package org.example.diccionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiccionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiccionarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/diccionario/DiccionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.diccionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiccionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
